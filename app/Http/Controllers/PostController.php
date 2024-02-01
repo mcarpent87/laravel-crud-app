21276a5ed2d89b1fd908ffe1fb92fc0d1c4ecad6
McLean Carpenter <mcarpent@McLeans-MacBook-Pro.local>
2024-01-31 20:38:47 -0700

    add routes for updating a post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,30 +7,48 @@ use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
-{   
-    public function delete(Post $post) {
-        if(auth()->user()->cannot('delete', $post)) {
-            return 'You cannot do that';
-        }
-        $post->delete();
+{
+    public function showEditForm(Post $post) {
+        return view('edit-post', ['post' => $post]);
+    }
 
+    public function actuallyUpdate(Post $post, Request $request) {
+        $incomingFields = $request->validate([
+            'title' => 'required',
+            'body' => 'required'
+        ]);
+
+        $incomingFields['title'] = strip_tags($incomingFields['title']);
+        $incomingFields['body'] = strip_tags($incomingFields['body']);
+
+        $post->update($incomingFields);
+
+        return back()->with('success', 'Post successfully updated.');
+    }
+    
+    public function delete(Post $post)
+    {
+        $post->delete();
         return redirect('/profile/' . auth()->user()->username)->with('success', 'Post successfully deleted.');
     }
 
-    public function showCreateForm() {
+    public function showCreateForm()
+    {
         return view('create-post');
     }
 
     //type hinting, laravel looks up the post based on the id value
-    public function viewSinglePost(Post $post) {
+    public function viewSinglePost(Post $post)
+    {
         $post['body'] = strip_tags(Str::markdown($post->body), '<p><ul><em><strong><ol><li><h3>');
         return view('single-post', ['post' => $post]);
     }
 
-    public function storeNewPost(Request $request) {
+    public function storeNewPost(Request $request)
+    {
         $incomingFields = $request->validate([
             'title' => 'required',
-            'body' => 'required'
+            'body' => 'required',
         ]);
 
         $incomingFields['title'] = strip_tags($incomingFields['title']);
