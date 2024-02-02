079943d1969d799df11fb7f1590dcd555ce38b72
McLean Carpenter <mcarpent@McLeans-MacBook-Pro.local>
2024-02-01 18:14:38 -0700

    implement avatars

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,26 +5,64 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
+use Intervention\Image\Facades\Image;
+use Illuminate\Support\Facades\Storage;
 
 class UserController extends Controller
-{   //use type hinting to lookup User model
-    public function profile(User $user) {
-        //pizza is the instance of the user model
-        return view('profile-posts', ['username' => $user->username, 'posts' => $user->posts()->latest()->get(), 'postCount'=> $user->posts()->count()]);
+{
+    public function storeAvatar(Request $request)
+    {
+        $request->validate([
+            'avatar' => 'required|image|max:6000',
+        ]);
+
+        $user = auth()->user();
+
+        $filename = $user->id . '-' . uniqid() . '.jpg';
+
+        $imgData = Image::make($request->file('avatar'))
+            ->fit(120)
+            ->encode('jpg');
+        Storage::put('public/avatars/' . $filename, $imgData);
+
+        $user->avatar = $filename; 
+        $user->save();
     }
 
-    public function showCorrectHomepage() {
-        if(auth()->check()) {
+    public function showAvatarForm()
+    {
+        return view('avatar-form');
+    }
+
+    //use type hinting to lookup User model
+    public function profile(User $user)
+    {
+        //pizza is the instance of the user model
+        return view('profile-posts', [
+            'username' => $user->username,
+            'avatar' => $user->avatar,
+            'posts' => $user
+                ->posts()
+                ->latest()
+                ->get(),
+            'postCount' => $user->posts()->count(),
+        ]);
+    }
+
+    public function showCorrectHomepage()
+    {
+        if (auth()->check()) {
             return view('homepage-feed');
         } else {
             return view('homepage');
         }
     }
-    
-    public function login(Request $request) {
+
+    public function login(Request $request)
+    {
         $incomingFields = $request->validate([
             'loginusername' => 'required',
-            'loginpassword' => 'required'
+            'loginpassword' => 'required',
         ]);
 
         if (auth()->attempt(['username' => $incomingFields['loginusername'], 'password' => $incomingFields['loginpassword']])) {
@@ -36,16 +74,18 @@ class UserController extends Controller
         }
     }
 
-    public function logout(){
+    public function logout()
+    {
         auth()->logout();
         return redirect('/')->with('success', 'You are now logged out.');
     }
 
-    public function register(Request $request) {
+    public function register(Request $request)
+    {
         $incomingFields = $request->validate([
             'username' => ['required', 'min:3', 'max:20', Rule::unique('users', 'username')],
             'email' => ['required', 'email', Rule::unique('users', 'email')],
-            'password' => ['required', 'min:8', 'confirmed']
+            'password' => ['required', 'min:8', 'confirmed'],
         ]);
         $user = User::create($incomingFields);
         auth()->login($user);
